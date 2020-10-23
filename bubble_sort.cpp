d24030d38a849eb9d464de05d8c682487377466e
Larissa Vasquez <larissavasquezz@outlook.com>
2020-10-23 13:42:53 -0300

    Troca de variável vetor sort

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,22 +1,22 @@
 #include<stdio.h>
 main(){
-	int vetor[10], i,j, aux;
+	int v[10], i,j, aux;
 	
 	printf("Entre com 10 digitos:\n");
 	for(i=0; i<10; i++){
 		printf("%dº :", i+1);
-		scanf("%d", &vetor[i]);					
+		scanf("%d", &v[i]);					
 	}
 	for(j=0; j<10; j++)
 		for(i=0; i<9; i++){// bubble só troca os dois termos
-			if(vetor[i]<vetor[i+1]){
-				aux = vetor[i];
-				vetor[i] = vetor[i+1];
-				vetor[i+1] = aux;
+			if(v[i]<v[i+1]){
+				aux = v[i];
+				v[i] = v[i+1];
+				v[i+1] = aux;
 			}			
 		}
-	printf("O vetor ordenado de forma decrescente e:\n");
+	printf("O v ordenado de forma decrescente e:\n");
 	for(i=0; i<10; i++){
-		printf("%d _ ", vetor[i]);				
+		printf("%d _ ", v[i]);				
 	}
 }
